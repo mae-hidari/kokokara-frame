e626fc62df4c88cf1a5a33845d993c2b04fac6d4
mae-hidari <katsuki.maemura@gmail.com>
2022-05-29 15:15:27 +0900

    feat:ng-zorro導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ja_JP } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ja from '@angular/common/locales/ja';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(ja);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: ja_JP }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
