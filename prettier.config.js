38a1bb7a3a008a6e5bd72f27cb95b01fc0b5f4c0
mae-hidari <katsuki.maemura@gmail.com>
2022-06-05 15:31:30 +0900

    feat: husky導入

---
+++ prettier.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  importOrder: [
+    '^@angular/(.*)$',
+    '^(.*)ngrx(.*)$',
+    '^(.*)module(.*)$',
+    '^(.*)service(.*)$',
+    '^(.*)guard(.*)$',
+    '^(.*)pipe(.*)$',
+    '^rxjs/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderParserPlugins: ['typescript', 'classProperties', '["decorators", { "decoratorsBeforeExport": true }]'],
+  parser: 'typescript',
+  printWidth: 140,
+};
