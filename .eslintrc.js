d18bab6b51a9aad9b1b19f81d0ef7fd3608bb86d
mae-hidari <katsuki.maemura@gmail.com>
2022-06-06 23:32:44 +0900

    feat: eslint, prettierの設定を調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,41 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'jasmine'],
   rules: {
-    'no-useless-constructor': 0,
+    'no-useless-constructor': 'off',
+    'import/no-duplicates': 'off',
+    camelcase: [
+      'off',
+      {
+        allow: ['ja_JP'],
+      },
+    ],
   },
   overrides: [
+    {
+      files: ['*.component.html'],
+      parser: '@angular-eslint/template-parser',
+      rules: {
+        '@typescript-eslint/dot-notation': 'off',
+        '@typescript-eslint/no-implied-eval': 'off',
+        '@typescript-eslint/no-throw-literal': 'off',
+        strict: 'off',
+        'import/first': 'off',
+        'lines-around-directive': 'off',
+        node: 'off',
+      },
+      plugins: ['@angular-eslint/template'],
+    },
+    {
+      files: ['*.html'],
+      plugins: ['html'],
+    },
     {
       files: ['*.spec.ts'],
       rules: {
         '@typescript-eslint/no-explicit-any': 'off',
         'jasmine/no-spec-dupes': 'off',
         'jasmine/no-suite-dupes': 'off',
+        'jasmine/new-line-before-expect': 'off',
       },
       plugins: ['jasmine'],
       extends: 'plugin:jasmine/recommended',
